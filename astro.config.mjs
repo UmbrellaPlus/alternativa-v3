6f63291861db80e04a66fb112fc1a3506e2b22ce
Linda <Linda>
2026-06-13 21:46:02 +0200

    readme upd + height fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://alternativa-life.com.ua/",
+  site: "https://alternativa-life.com.ua/",
   integrations: [sitemap()],
 });
