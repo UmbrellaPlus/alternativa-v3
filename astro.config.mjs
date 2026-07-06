1b38380f8b321f3d90d897186a5ef76c51c416c4
Linda <Linda>
2026-07-06 23:44:39 +0200

    header, footer optim, font added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -7,4 +7,9 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://alternativa-life.com.ua/",
   integrations: [sitemap()],
+  fonts:[{
+    provider: fontProviders.google(),
+    name: "Montserrat",
+    cssVariable: "--font-montserrat",
+  }]
 });
